4cd14d0728e702e40d100a4d8544c34de3822fe1
Jeroen Grooten <contact@jeroengrooten.nl>
2015-11-16 23:01:13 +0100

    'hoofdbestanden' van de extensie
    De bestanden van de extensie, elk bestand is nodig.

---
+++ script.js
@@ -0,0 +1,250 @@
+//Klas 1 - 2MI1A
+document.getElementById("klas2MI1A").onclick = klas2MI1A;
+function klas2MI1A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00001.htm";
+} 
+
+//Klas 2 - 2MI1B
+document.getElementById("klas2MI1B").onclick = klas2MI1B;
+function klas2MI1B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00002.htm";
+} 
+
+//Klas 3 - 2MI2A
+document.getElementById("klas2MI2A").onclick = klas2MI2A;
+function klas2MI2A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00003.htm";
+} 
+
+//klas 4 - 3MB1A
+document.getElementById("klas3MB1A").onclick = klas3MB1A;
+function klas3MB1A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00004.htm";
+} 
+
+//Klas 5 - 3MB2A
+document.getElementById("klas3MB2A").onclick = klas3MB2A;
+function klas3MB3A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00005.htm";
+} 
+
+//Klas 6 - 3MB3A
+document.getElementById("klas3MB3A").onclick = klas3MB3A;
+function klas3MB3A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00006.htm";
+}
+
+//Klas 7 - 4IB1A
+document.getElementById("klas4IB1A").onclick = klas4IB1A;
+function klas4IB1A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00007.htm";
+}
+
+//Klas 8 - 4IB1B
+document.getElementById("klas4IB1B").onclick = klas4IB1B;
+function klas4IB1B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00008.htm";
+}
+
+//Klas 9 - 4IB2A
+document.getElementById("klas4IB2A").onclick = klas4IB2A;
+function klas4IB2A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00009.htm";
+}
+
+//Klas 10 - 4IB3A
+document.getElementById("klas4IB3A").onclick = klas4IB3A;
+function klas4IB3A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00010.htm";
+}
+
+//Klas 11 - 4IB4A
+document.getElementById("klas4IB4A").onclick = klas4IB4A;
+function klas4IB4A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00011.htm";
+}
+
+//Klas 12 - 4NB3A
+document.getElementById("klas4NB3A").onclick = klas4NB3A;
+function klas4NB3A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00012.htm";
+}
+
+//Klas 13 - 4NB4A
+document.getElementById("klas4NB4A").onclick = klas4NB4A;
+function klas4NB4A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00013.htm";
+}
+
+//Klas 14 - 4AM1A
+document.getElementById("klas4AM1A").onclick = klas4AM1A;
+function klas4AM1A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00014.htm";
+}
+
+//Klas 15 - 4AM1B
+document.getElementById("klas4AM1B").onclick = klas4AM1B;
+function klas4AM1B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00015.htm";
+}
+
+//Klas 16 - 4AM2A
+document.getElementById("klas4AM2A").onclick = klas4AM2A;
+function klas4AM2A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00016.htm";
+}
+
+//Klas 17 - 4AM3A
+document.getElementById("klas4AM3A").onclick = klas4AM3A;
+function klas4AM3A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00017.htm";
+}
+
+//Klas 18 - 4AM4A
+document.getElementById("klas4AM4A").onclick = klas4AM4A;
+function klas4AM4A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00018.htm";
+}
+
+//Klas 19 - YTWK123
+document.getElementById("klasYTWK123").onclick = klasYTWK123;
+function klasYTWK123(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00019.htm";
+}
+
+//Klas 20 - 4ENG1
+document.getElementById("klas4ENG1").onclick = klas4ENG1;
+function klas4ENG1(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00020.htm";
+}
+
+//Klas 21 - 4ENG2
+document.getElementById("klas4ENG2").onclick = klas4ENG2;
+function klas4ENG2(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00021.htm";
+}
+
+//Klas 22 - 4IT1
+document.getElementById("klas4IT1").onclick = klas4IT1;
+function klas4IT1(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00022.htm";
+}
+
+//Klas 23 - 4BM1
+document.getElementById("klas4BM1").onclick = klas4BM1;
+function klas4BM1(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00023.htm";
+}
+
+//Klas 24 - 4BR1
+document.getElementById("klas4BR1").onclick = klas4BR1;
+function klas4BR1(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00024.htm";
+}
+
+//Klas 25 - 4IT2
+document.getElementById("klas4IT2").onclick = klas4IT2;
+function klas4IT2(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00025.htm";
+}
+
+//Klas 26 - 4IT3
+document.getElementById("klas4IT3").onclick = klas4IT3;
+function klas4IT3(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00026.htm";
+}
+
+//Klas 27 - 4BM3
+document.getElementById("klas4BM3").onclick = klas4BM3;
+function klas4BM3(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00027.htm";
+}
+
+//Klas 28 - 4BR2
+document.getElementById("klas4BR2").onclick = klas4BR2;
+function klas4BR2(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00028.htm";
+}
+
+//Klas 29 - 4BV2
+document.getElementById("klas4BV2").onclick = klas4BV2;
+function klas4BV2(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00029.htm";
+}
+
+//Klas 30 - 4BV3
+document.getElementById("klas4BV3").onclick = klas4BV3;
+function klas4BV3(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00030.htm";
+}
+
+//Klas 31 - 4ENG3
+document.getElementById("klas4ENG3").onclick = klas4ENG3;
+function klas4ENG3(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00031.htm";
+}
+
+//Klas 32 - 4ENG4
+document.getElementById("klas4ENG4").onclick = klas4ENG4;
+function klas4ENG4(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00032.htm";
+}
+
+//Klas 33 - 4BR3
+document.getElementById("klas4BR3").onclick = klas4BR3;
+function klas4BR3(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00033.htm";
+}
+
+//Klas 34 - 4BV1A
+document.getElementById("klas4BV1A").onclick = klas4BV1A;
+function klas4BV1A(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00034.htm";
+}
+
+//Klas 35 - 4BV1B
+document.getElementById("klas4BV1B").onclick = klas4BV1B;
+function klas4BV1B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00035.htm";
+}
+
+//Klas 36 - BM2
+document.getElementById("klasBM2").onclick = klasBM2;
+function klasBM2(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00036.htm";
+}
+
+//Klas 37 - B4
+document.getElementById("klasB4").onclick = klasB4;
+function klasB4(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00037.htm";
+}
+
+//Klas 38 - 4AM4B
+document.getElementById("klas4AM4B").onclick = klas4AM4B;
+function klas4AM4B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00038.htm";
+}
+
+//Klas 39 - 4AM3B
+document.getElementById("klas4AM3B").onclick = klas4AM3B;
+function klas4AM3B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00039.htm";
+}
+//Klas 40 - 4AM2B
+document.getElementById("klas4AM2B").onclick = klas4AM2B;
+function klas4AM2B(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00040.htm";
+}
+
+//Klas 41 - 2MI2B
+document.getElementById("klas2MI2B").onclick = klas2MI2B;
+function klas(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00041.htm";
+}
+
+//Klas 42 - YISTDV-S
+document.getElementById("klasYISTDV").onclick = klasYISTDV;
+function klasYISTDV(){
+    document.getElementById("myFrame").src = "http://roosters.mboutrecht.nl/TEC/roosters/47/c/c00042.htm";
+}
