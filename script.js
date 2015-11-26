6caad2dae23fcb0d5ba0505d61a9c7da156c307b
Jeroen Grooten <grootenjeroen@hotmail.com>
2015-11-26 21:20:43 +0100

    Rooster src wordt nu elke week geupdate
    Weeknummer wordt nu in src link gezet zodat het rooster up to date blijft.

--- script.js
+++ script.js
@@ -18,9 +18,16 @@ document.getElementById("reset").onclick = function() {
     choiceIsSet(false);
 };
 
+//Huidige week nummer in src link zetten
+var currentWeekNumber = (function(date){
+    date.setHours(0,0,0);
+    date.setDate(date.getDate()+4-(date.getDay()||7));
+    return Math.ceil((((date - new Date(date.getFullYear(),0,1))/8.64e7)+1)/7);
+})(new Date());
+
 // De rest
 function makePageURL(page) {
-    return "http://roosters.mboutrecht.nl/TEC/roosters/47/c/" + page + '.htm';
+    return "http://roosters.mboutrecht.nl/TEC/roosters/" + currentWeekNumber + "/c/" + page + '.htm';
 }
 
 var classes = {
@@ -85,4 +92,3 @@ if (choice) { // Wanneer er een keuze is gemaakt ...
     // ... zet de iframe op de juiste rooster-pagina en haal de klassen knoppen weg.
     choiceIsSet(true, makePageURL(choice));
 }
-
